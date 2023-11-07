647047feee0385085efa6ee5f78a645565dafce5
Angelk90 <angelo.k90@hotmail.it>
2023-11-07 14:35:43 +0100

    Change

--- server_nodejs/index.js
+++ server_nodejs/index.js
@@ -1,12 +1,33 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 8080;
 let c = 0;
 
-http
-  .createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
     c++;
-    console.log("C:", c);
-    res.write(`C: ${c}`);
+    //console.log(`${method} ${req.url} - C: ${c}`);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ count: c }));
     res.end();
-  })
-  .listen(8080);
+  } else {
+    //console.log(`${method} ${req.url} - Invalid request`);
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ error: "Invalid request" }));
+    res.end();
+  }
+});
+
+server.on("error", function (error) {
+  console.error("Server error:", error);
+  res.writeHead(500, { "Content-Type": "text/plain" });
+  res.write("Internal Server Error");
+  res.end();
+});
+
+server.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}`);
+});
